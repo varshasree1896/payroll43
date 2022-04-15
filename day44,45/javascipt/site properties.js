ae7cb4fa79fb654e738903eda68721f6dfc7f596
Pooja Sree <you@example.com>
2022-04-15 17:40:47 +0530

    Added welcome message

---
+++ day44,45/javascipt/site properties.js
@@ -0,0 +1,6 @@
+const site_properties = {
+    use_local_storage: "true",
+    home_page: "../pages/home.html",
+    add_employee_payroll_page: "../pages/payroll_form.html",
+    server_url: "http://127.0.0.1:3000/EmployeePayrollDB"
+  };
